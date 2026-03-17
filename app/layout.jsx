d29e10e36f70d9e58b9c027193c8d3e2754f5fcd
Govind Chauhan <delivery@auotam.com>
2026-03-17 21:42:19 +0530

    updating favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,8 +12,8 @@ export const metadata = {
   },
   description: 'XCELERATED performance training website.',
   icons: {
-    icon: '/favicon-2026.svg',
-    shortcut: '/favicon-2026.svg',
+    icon: '/favicon-new.svg',
+    shortcut: '/favicon-new.svg',
   },
 };
 
